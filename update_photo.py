aba79b371e79235a61d54e3c43b623a4794372c5
Zese07 <128886474+Zese07@users.noreply.github.com>
2023-04-25 21:32:06 +0800

    Update update_photo.py

--- update_photo.py
+++ update_photo.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import random
 
+
 image_files = ['0.png', '1.png', '3.png', '7.png']
 new_image_filename = random.choice(image_files)
 new_image = Image.open(new_image_filename)
@@ -8,5 +9,3 @@ new_image = Image.open(new_image_filename)
 photo = Image.open('photo.png')
 photo = new_image.copy()
 photo.save('photo.png')
-
-print("Photo updated successfully!")
